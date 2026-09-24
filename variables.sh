867000e64a8f6de1eb9fe9a482802968fd60815e
Venu Kunreddy (CW) <Venu.Kunreddy@jda.com>
2026-09-24 05:40:28 +0530

    hello world

--- variables.sh
+++ variables.sh
@@ -5,5 +5,5 @@ person2=sachin
 
 echo "$person1 :: Hi $person2 how are you"
 echo "$person2 :: Hi $person1 I am fine,how are you"
-echo "$person1 :: I am fine thank you $person1 how is your work"
+echo "$person1 :: I am fine thank you $person2 how is your work"
 echo "$person2 : : I am thinking to upgrde to devops"
