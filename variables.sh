35d97ca798c1cb26b478f31918f2d96326631992
Venu Kunreddy (CW) <Venu.Kunreddy@jda.com>
2026-09-24 05:34:28 +0530

    hello world

---
+++ variables.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+
+echo "ramesh :: Hi suresh how are you"
+echo "suresh :: Hi ramesh I am fine,how are you"
+echo "ramesh :: I am fine thank you how is your work"
+echo "sursh : : I am thinking to upgrde to devops"
