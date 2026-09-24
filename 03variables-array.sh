91ad4b188e94f4bf0a720056e4faa089c5cdeb18
Venu Kunreddy (CW) <Venu.Kunreddy@jda.com>
2026-09-24 06:22:19 +0530

    hello world

---
+++ 03variables-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DJTillu" "Murari" "Bahubali")
+
+echo "first movie is: ${MOVIES[0]}"
+
+echo "second movie is: ${MOVIES[1]}"
+
+echo "third movie is: ${MOVIES[2]}"
+
+echo "list of movies are ${MOVIES[@]}"
