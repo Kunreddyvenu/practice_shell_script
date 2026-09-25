79a93e2244ec71ca35d32b691e770c3c56f9e4b2
Venu Kunreddy (CW) <Venu.Kunreddy@jda.com>
2026-09-25 05:45:31 +0530

    condition script

--- 07installation-with-conditions.sh
+++ 07installation-with-conditions.sh
@@ -5,6 +5,8 @@ dnf install mysql -y
 if [ $? -ne 0 ]
 then
     echo "you are not a super user"
+    exit 1
+# if you want to exit here it self you can use exit command like exit 1-127
 else 
     echo "you are a super user"
 fi
